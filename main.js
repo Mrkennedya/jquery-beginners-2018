699d39340bc4eea057093ce6f1d50cbfd9430685
Mrkennedya <angel.kennedy@ufl.edu>
2018-11-11 08:09:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+$(document).ready(() => {
+
+  $('#crossroads-hide').on('click', () => {
+    $('.crossroads').hide();
+  });
+
+  $('#crossroads-show').on('click', () => {
+    $('.crossroads').show();
+  });
+
+  $('#fireBreath-hide').on('click', () => {
+    $('.fireBreath').hide();
+  });
+
+  $('#fireBreath-show').on('click', () => {
+    $('.fireBreath').show();
+  });
+
+  $('#meetup-hide').on('click', () => {
+    $('.meetup').hide();
+  });
+
+  $('#meetup-show').on('click', () => {
+    $('.meetup').show();
+  });
+
+  $('#spinning-hide').on('click', () => {
+    $('.spinning').hide();
+  });
+
+  $('#spinning-show').on('click', () => {
+    $('.spinning').show();
+  });
+
+
+
+});
